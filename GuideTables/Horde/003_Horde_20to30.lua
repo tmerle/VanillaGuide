9fdff9299d545db635a74dd84082a209ac3d8649
Lanjelin <lanjelin.svanes@gmail.com>
2017-02-06 13:50:36 +0100

    Update 003_Horde_20to30.lua

--- GuideTables/Horde/003_Horde_20to30.lua
+++ GuideTables/Horde/003_Horde_20to30.lua
@@ -10,6 +10,8 @@ Description: Guide Serie - 003 From Lvl 20 to Lvl 30
             from level 20 to lever 30
     1.04.2
     	-- no changes in here for this revision
+    1.04.3
+	-- no changes in here for this revision
 ------------------------------------------------------
 Connection:
 --]]--------------------------------------------------
