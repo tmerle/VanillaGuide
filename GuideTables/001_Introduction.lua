ae3d05f79850593c36d79dde0269d1371f98e2cf
Lanjelin <lanjelin.svanes@gmail.com>
2017-02-12 11:08:37 +0100

    Add files via upload

--- GuideTables/001_Introduction.lua
+++ GuideTables/001_Introduction.lua
@@ -10,7 +10,7 @@ Description: Guide Serie - 001 Introduction
     1.04.2
         -- no changes in here for this revision
 	1.04.3
-		-- no changes in here for this revision
+		-- Changed to reflect the changes in color codes.
 ------------------------------------------------------
 Connection:
 --]]--------------------------------------------------
@@ -40,7 +40,7 @@ Table_001_Introduction = {
             [12] = { str = "#INTurn in a quest.#" },
 			[13] = { str = "#SKIPSkip a quest.#" },			
             [14] = { str = "#NPCNPC/Mob.#" },
-            [15] = { str = "#ITEMAn item.#" },
+            [15] = { str = "#ITEMAn item or an object.#" },
             [16] = { str = "#COORDCoordinates [15,26].#" },
             [17] = { str = "." },
             [18] = { str = "#GETBINDABLE KEYS:#  There are bindable keys for the \"Prev\" and \"Next\" Steps and Guides. You can find them in the key bindings menu" },
